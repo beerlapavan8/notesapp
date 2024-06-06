13092629dd1f14797927068b48f611d91f9b0d1e
pavab <beerlapavan4@gmail.com>
2024-06-06 11:54:30 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use(express.json());
 
 
